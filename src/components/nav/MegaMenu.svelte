7c1c0050e6cb4426ed242c72a92279c0a441983e
Callum Harrod <callumharrod1994@hotmail.co.uk>
2022-05-25 20:31:03 +0100

    Mega menu fuckers

---
+++ src/components/nav/MegaMenu.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import WalletActivity from '$icons/WalletActivity.svelte'
+</script>
+
+<div class="absolute top-full left-0 px-5 w-full py-10 bg-black min-h-[200px] border-t border-solid border-t-zilkroad-gray-dark">
+  <div class="flex items-start w-full max-w-screen-xl mx-auto">
+
+    <div class="grid grid-cols-2 grid-flow-row col-gap gap-5">
+
+      <a href="/wallet-activity" class="w-[224px] p-5 border border-solid border-zilkroad-gray-dark rounded-lg hover:bg-zilkroad-gray-dark">
+        <div class="flex items-center mb-3">
+          <WalletActivity /><span class="ml-2">Wallet activity</span>
+        </div>
+        <p class="text-[14px] leading-[1.5] text-zilkroad-gray-lighter">
+          View what’s going on with the most popular wallets.
+        </p>
+      </a>
+
+      <a href="/wallet-activity" class="w-[224px] p-5 border border-solid border-zilkroad-gray-dark rounded-lg hover:bg-zilkroad-gray-dark">
+        <div class="flex items-center mb-3">
+          <WalletActivity /><span class="ml-2">Launchpad</span>
+        </div>
+        <p class="text-[14px] leading-[1.5] text-zilkroad-gray-lighter">
+          View what’s going on with the most popular wallets.
+        </p>
+      </a>
+
+      <a href="/wallet-activity" class="w-[224px] p-5 border border-solid border-zilkroad-gray-dark rounded-lg hover:bg-zilkroad-gray-dark">
+        <div class="flex items-center mb-3">
+          <WalletActivity /><span class="ml-2">Collections</span>
+        </div>
+        <p class="text-[14px] leading-[1.5] text-zilkroad-gray-lighter">
+          View what’s going on with the most popular wallets.
+        </p>
+      </a>
+
+      <a href="/wallet-activity" class="w-[224px] p-5 border border-solid border-zilkroad-gray-dark rounded-lg hover:bg-zilkroad-gray-dark">
+        <div class="flex items-center mb-3">
+          <WalletActivity /><span class="ml-2">Docs</span>
+        </div>
+        <p class="text-[14px] leading-[1.5] text-zilkroad-gray-lighter">
+          View what’s going on with the most popular wallets.
+        </p>
+      </a>
+    </div>
+
+    <div class="rounded-lg min-h-[256px] w-full bg-[url('/static/images/cta-background.png')] flex flex-col flex-1 ml-5 p-10">
+      <h3 class="text-white text-2xl mb-5">Advertise your collection</h3>
+      <p class="w-full mb-5">Make your collection stand out by becoming a featured collection on Zilkroad. You will get featured banners, the featured tick and more by featuring your collection.</p>
+      <a href="/" class="bg-white text-[#989898] px-5 rounded-xl h-12 inline-flex items-center self-start">Feature your collection</a>
+    </div>
+
+  </div>
+</div>
--- src/components/nav/Navbar.svelte
+++ src/components/nav/Navbar.svelte
@@ -1,21 +1,39 @@
 <script lang="ts">
   import ConnectWallet from '../ConnectWallet.svelte'
   import Zilkroad from '$icons/Zilkroad.svelte'
+  import Chevron from '$icons/Chevron.svelte'
   import Searchbox from '../Searchbox.svelte'
+  import MegaMenu from './MegaMenu.svelte';
   export let light = false
+  let megaMenuShow = false;
+
+  function handleMegaShow() {
+    megaMenuShow = true;
+  }
+
+  function handleMegaHide() {
+    megaMenuShow = false;
+  }
+
 </script>
 
 <nav
-  class="items-center justify-center hidden w-full max-w-screen-xl px-4 py-5 mx-auto space-x-6 md:flex lg:space-x-10 2xl:px-0"
+  class="relative bg-black px-5" on:mouseleave={handleMegaHide} on:blur={handleMegaHide}
 >
-  <a href="/" class="flex items-center">
-    <Zilkroad />
-    <span class="text-xl hidden lg:block font-medium ml-3 leading-[0.8]"> Zilkroad </span>
-  </a>
-  <a href="/" class="text-zilkroad-gray-lighter font-light lg:!ml-20">Home</a>
-  <a href="/collections" class="font-light text-zilkroad-gray-lighter">Collections</a>
-  <a href="/marketplace" class="font-light text-zilkroad-gray-lighter">Marketplace</a>
-  <a href="/launchpad" class="font-light text-zilkroad-gray-lighter">Launchpad</a>
-  <Searchbox className="lg:!ml-auto" />
-  <ConnectWallet />
+  <div class="items-center justify-between hidden w-full max-w-screen-xl py-5 lg:mx-auto md:flex 2xl:px-0 ">
+    <a href="/" class="flex items-center" on:mouseenter={handleMegaHide}>
+      <Zilkroad />
+      <span class="text-xl hidden lg:block font-medium ml-3 leading-[0.8]"> Zilkroad </span>
+    </a>
+    <a href="/" class="text-zilkroad-gray-lighter inline-flex items-center justify-center font-light lg:!ml-20 lg:pl-5 lg:pr-5 h-12 hover:text-white" on:mouseenter={handleMegaHide}>Home</a>
+    <a href="/marketplace" class="font-light text-zilkroad-gray-lighter inline-flex items-center justify-center lg:pl-5 lg:pr-5 h-12 hover:text-white" on:mouseenter={handleMegaHide}>Marketplace</a>
+    <button class="font-light text-zilkroad-gray-lighter inline-flex items-center justify-center lg:pl-5 lg:pr-5 h-12 hover:text-white" on:mouseenter={handleMegaShow}><span class="mr-2">Explore</span> <Chevron /></button>
+    <div on:mouseenter={handleMegaHide} class="flex items-center lg:!ml-auto">
+      <Searchbox />
+      <ConnectWallet/>
+    </div>
+  </div>
+  {#if megaMenuShow}
+    <MegaMenu />
+  {/if}
 </nav>
