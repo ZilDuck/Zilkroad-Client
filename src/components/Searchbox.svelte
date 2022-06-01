377cbaec556279e206f8d9ec58bfe454e3de0314
Nic <nicfusc@gmail.com>
2022-06-01 21:47:31 +0100

    [chore] Search response

--- src/components/Searchbox.svelte
+++ src/components/Searchbox.svelte
@@ -1,10 +1,29 @@
-<script lang="ts">
+<script>
   export let light = false
   export let className = ''
+  
+  let query = ''
+  let results = ''
+  
+  async function search() {
+    let search = await fetch(`/search/${query}/search.json`).catch(error => {
+      console.log(error)
+    }).then((r) => r.json())
+    results = JSON.stringify(search)
+  }
+  
 </script>
+<div class="relative">
+  <input
+    type="text"
+    class="w-full md:w-60 h-12 rounded-lg p-4 bg-zilkroad-gray-dark text-white {className}"
+    placeholder="Search"
+    bind:value={query}
+  />
+  <button class="text-zilkroad-teal" on:click={search}>Search</button>
 
-<input
-  type="text"
-  class="w-full md:w-60 h-12 rounded-lg p-4 bg-zilkroad-gray-dark text-white {className}"
-  placeholder="Search"
-/>
+  <pre class="absolute top-16 w-52 p-4 bg-amber-50 text-zilkroad-gray-dark text-xs">{results}</pre>
+
+</div>
+
+
