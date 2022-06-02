e399da988141102c9f17461cb3799b2897f08265
Nic <nicfusc@gmail.com>
2022-06-02 15:16:04 +0100

    [chore] Added Components for the search results

---
+++ src/lib/SearchResultList/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import SearchResult from "./SearchResult.svelte";
+  export let searchResults;
+</script>
+
+{#each searchResults as searchResult}
+  <SearchResult {searchResult} />
+{/each}
