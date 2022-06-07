9246c3e14450059379311ec3793ba92fefac28a5
Nic <nicfusc@gmail.com>
2022-06-07 20:33:11 +0100

    [chore] Made data reactive and image conditional

--- src/lib/SearchResultList/SearchResult.svelte
+++ src/lib/SearchResultList/SearchResult.svelte
@@ -1,12 +1,14 @@
 <script>
   export let searchResult
 
-  let imageSrc = searchResult?.result_image
-  let resultAction = searchResult?.result_action
-  let resultTitle = searchResult?.result_text
+  $:imageSrc = searchResult.result_image ?? '/images/nft-image.png'
+  $:resultAction = searchResult.result_action ?? ''
+  $:resultTitle = searchResult.result_text ?? ''
 </script>
 
 <div class="flex items-center space-x-3 mb-5 last:mb-0">
-  <img src="/images/nft-image.png" alt="Search result" class="max-w-[40px] rounded-lg" />
-  <p><a href={resultAction}>Name of search result</a></p>
+  {#if imageSrc}
+    <img src={imageSrc} alt="Search result" class="max-w-[40px] rounded-lg" />
+  {/if}
+  <p><a href={resultAction}>{resultTitle}</a></p>
 </div>
