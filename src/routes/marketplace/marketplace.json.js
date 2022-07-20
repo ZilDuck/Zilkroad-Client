cb325482968ecac376f1d013357427aa85670f02
Nic <nicfusc@gmail.com>
2022-07-20 21:24:34 +0100

    [chore] Send collection address to filter marketplace by collection. [todo] Added a temporary placeholder for a missing variable.

--- src/routes/marketplace/marketplace.json.js
+++ src/routes/marketplace/marketplace.json.js
@@ -1,8 +1,11 @@
 import * as api from "../../lib/api.js";
 
-export async function get() {
+export async function get({ url: { searchParams } }) {
   
-  const marketplace = await api.get(`marketplace`)
+  const page = searchParams.get('page') ?? 1
+  const collection = searchParams.get('collection') ?? ''
+
+  const marketplace = await api.get(`marketplace?page=${page}&contract=${collection}`)
   
   return {
     body: {
