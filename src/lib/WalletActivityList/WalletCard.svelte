68c41fcb34076b79e4cfecf09cbe9a76abf9fd20
richardbadman <richard.a.g@hotmail.co.uk>
2022-08-01 19:04:41 +0100

    [feat] Add royalties to home page

--- src/lib/WalletActivityList/WalletCard.svelte
+++ src/lib/WalletActivityList/WalletCard.svelte
@@ -1,3 +1,8 @@
+<script>
+  export let wallet = []
+  const shortenedAddress = wallet.address.substring(0,9)
+</script>
+
 <div class="flex flex-auto flex-col rounded-lg bg-zilkroad-gray-dark p-5 mb-5">
   <div class="flex align-center pb-5">
     <img
@@ -5,11 +10,11 @@
       class="inline w-4 rounded-xl self-center"
       src="https://i.pickadummy.com/100x100?cache="
     />
-    <p class="text-white pl-[10px] underline">zil16wg79q...</p>
+    <p class="text-white pl-[10px] underline ">{wallet.address}</p>
   </div>
   <hr class="border-zilkroad-gray-border" />
   <p class="pt-5 ">
     <span class="text-gray-400">Total royalties USD</span>
   </p>
-  <p>$1200~</p>
+  <p>${wallet.total_usd}~</p>
 </div>
