99cd2b5a708d96f95b97a222b940c5e9d29e9cee
Nic <nicfusc@gmail.com>
2022-08-08 21:31:34 +0100

    [chore] Convert to string incase of 0.

--- src/lib/fungibles.js
+++ src/lib/fungibles.js
@@ -2,7 +2,7 @@ export function convertWithDecimals (approvedFungibles, fungibleAddressOrSymbol,
   for (let i = 0; i < approvedFungibles.length; i++) {
     if (
       approvedFungibles[i].fungible_address === fungibleAddressOrSymbol ||
-      approvedFungibles[i].fungible_symbol.toLowerCase() === fungibleAddressOrSymbol.toLowerCase()
+      approvedFungibles[i].fungible_symbol.toLowerCase() === fungibleAddressOrSymbol.toString().toLowerCase()
     ) {
       if (reverse) {
         sellAmount = sellAmount / Math.pow(10, approvedFungibles[i].decimals)
