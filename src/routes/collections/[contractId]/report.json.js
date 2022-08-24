59ee3ff981927cc342ce1625ab35870d71b56473
richardbadman <richard.a.g@hotmail.co.uk>
2022-08-24 20:18:13 +0100

    [feat] Implement report functionality
    This also uses toasts :>

---
+++ src/routes/collections/[contractId]/report.json.js
@@ -0,0 +1,8 @@
+import * as api from '../../../lib/api.js'
+
+export async function get({ params, url: { searchParams } }) {
+  const { contractId } = params
+  const user = searchParams.get('user')
+  
+  return await api.get(`report/${contractId}/${user}`)
+}
