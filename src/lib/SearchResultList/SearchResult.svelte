50722f2a1b41ab0b2c413339b885d2dc47b6a467
richardbadman <richard.a.g@hotmail.co.uk>
2022-10-11 21:25:10 +0100

    [Fix] Fix search bar
    With the introduction of search vectors, we now have better search (it
    isn't the best, but better than what we had)

--- src/lib/SearchResultList/SearchResult.svelte
+++ src/lib/SearchResultList/SearchResult.svelte
@@ -1,9 +1,18 @@
 <script>
-  export let searchResult
+  import { cdnBaseUrl } from '../../lib/cdn'
 
-  $:imageSrc = searchResult.result_image ?? '/images/nft-image.png'
+  export let searchResult
+  
+  // Leaving this in, just in case Nic hates me and I've changed things
+  // $:imageSrc = searchResult.result_image ?? '/images/nft-image.png'
   $:resultAction = searchResult.result_action ?? ''
   $:resultTitle = searchResult.result_text ?? ''
+  let imageSrc = '/images/nft-image.png'
+  if ( searchResult.result_action !== '' && searchResult.result_text !== '' ) {
+    imageSrc = cdnBaseUrl + searchResult.nonfungible_address + '?optimizer=image&width=800'
+  } else {
+    imageSrc = ''
+  }
   
   let placeholder = '/images/nft-image.png'
 
