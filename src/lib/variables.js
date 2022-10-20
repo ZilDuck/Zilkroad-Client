b4120f461dc79ee73209ab5848552854cb268904
Nic <nicfusc@gmail.com>
2022-10-20 18:35:49 +0100

    [chore] Update marketplace contract

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,7 +1,7 @@
 export const variables = {
   apiEndpoint: import.meta.env.VITE_API_ENDPOINT ?? process.env.VITE_API_ENDPOINT,
   network: import.meta.env.VITE_BLOCK_NETWORK ?? process.env.VITE_BLOCK_NETWORK,
-  marketplaceAddress: '0x7ff10124194f23e7478614026c125b8a9fd74bcf',
+  marketplaceAddress: '0xad43392d1750771e58931801ddc274e5984660b4',
   wZilAddress: '0x864895d52504c388A345eF6cd9C800DBBD0eF92A',
   cdnBaseUrl: import.meta.env.VITE_CDN_BASE_URL ?? process.env.VITE_CDN_BASE_URL
 }
