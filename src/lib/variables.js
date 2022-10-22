d05a096a346a102a41bce334cf32b4adcdf67890
richardbadman <richard.a.g@hotmail.co.uk>
2022-10-22 20:54:10 +0100

    make static

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -4,6 +4,6 @@ export const variables = {
   marketplaceAddress: '0xad43392d1750771e58931801ddc274e5984660b4',
   wZilAddress: '0x864895d52504c388A345eF6cd9C800DBBD0eF92A',
   cdnBaseUrl: import.meta.env.VITE_CDN_BASE_URL ?? process.env.VITE_CDN_BASE_URL,
-  taxAmount: import.meta.env.VITE_TAX_AMOUNT ?? process.env.VITE_TAX_AMOUNT,
-  maxRoyaltyBps: import.meta.env.VITE_MAX_ROYALTY_BPS ?? process.env.VITE_MAX_ROYALTY_BPS
+  taxAmount: 200,
+  maxRoyaltyBps: 10000
 }
