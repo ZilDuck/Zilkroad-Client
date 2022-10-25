0220e35c4145d85189e25c870fc0e32808afa34c
Elliott Green <eg145@brighton.ac.uk>
2022-10-25 13:25:23 +0100

    [feat] stub some working logic
    https://geoffrich.net/posts/svelte-social-image/
    https://github.com/geoffrich/sveltekit-og-post

---
+++ src/routes/opengraph-og/index.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Card from '../../lib/OpenGraph/OpenGraphCard.svelte';
+
+	const mess = 'fuck you'
+</script>
+
+<svelte:head>
+	<meta content="http://localhost:3000/opengraph-og?message=Welcome!" property="og:image" />
+</svelte:head>
+
+<div class="card">
+	<Card message={mess}/>
+</div>
+
+<style>
+	@font-face {
+		font-family: 'Noto Sans';
+		src: url('/src/lib/OpenGraph/NotoSans-Regular.ttf');
+	}
+
+	.card {
+		height: 630px;
+		width: 1200px;
+	}
+</style>
