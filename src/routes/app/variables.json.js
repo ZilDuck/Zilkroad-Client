57d4efbcc0e389f4e49747e058dcacac99af5049
Nic <nicfusc@gmail.com>
2022-12-15 20:54:36 +0000

    [chore] Get variables from node for builds and .env for local

--- src/routes/app/variables.json.js
+++ src/routes/app/variables.json.js
@@ -1,8 +1,13 @@
+/**
+ * process.env should be what gets set during runtime in node on remote server.
+ * import.meta.env should be what is available in the .env file for local dev.
+ * 
+ * @returns {Promise<{body: {network: (*|string|boolean)}}>}
+ */
 export async function get() {
   return {
     body: {
-      "process.env": process.env,
-      "import.meta.env": import.meta.env
+      network: import.meta.env.VITE_BLOCK_NETWORK ?? process.env.VITE_BLOCK_NETWORK
     } 
   }
 }
