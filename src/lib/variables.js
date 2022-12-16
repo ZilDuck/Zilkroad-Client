427aa7c1ec6d3dac569cd3aa3f8731373126ac50
Elliott Green <eg145@brighton.ac.uk>
2022-12-16 22:00:09 +0000

    [chore] unfuck vars

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -4,6 +4,6 @@ export const variables = {
   marketplaceAddress: import.meta.env.VITE_MARKETPLACE_ADDRESS ?? process.env.VITE_MARKETPLACE_ADDRESS,
   wZilAddress: import.meta.env.VITE_WZIL_ADDRESS ?? process.env.VITE_WZIL_ADDDRESS,
   cdnBaseUrl: import.meta.env.VITE_CDN_BASE_URL ?? process.env.VITE_CDN_BASE_URL,
-  maxRoyaltyBps: '10000',
-  taxAmount: '200'
+  maxRoyaltyBps: import.meta.env.VITE_MAX_ROYALTY_BPS ?? process.env.VITE_MAX_ROYALTY_BPS,
+  taxAmount: import.meta.env.VITE_TAX_AMOUNT ?? process.env.VITE_TAX_AMOUNT
 }
