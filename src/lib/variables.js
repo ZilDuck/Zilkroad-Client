1cb61ccdb407c6fc436f62ffd2cd5c60eb5ae960
prodpeak <dtudor01@gmail.com>
2023-01-13 00:28:45 +0000

    Remove variables from build

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,9 +1,9 @@
 export const variables = {
-  apiEndpoint: import.meta.env.VITE_API_ENDPOINT ?? process.env.VITE_API_ENDPOINT,
-  network: import.meta.env.VITE_BLOCK_NETWORK ?? process.env.VITE_BLOCK_NETWORK,
-  marketplaceAddress: import.meta.env.VITE_MARKETPLACE_ADDRESS ?? process.env.VITE_MARKETPLACE_ADDRESS,
-  wZilAddress: import.meta.env.VITE_WZIL_ADDRESS ?? process.env.VITE_WZIL_ADDDRESS,
-  cdnBaseUrl: import.meta.env.VITE_CDN_BASE_URL ?? process.env.VITE_CDN_BASE_URL,
-  maxRoyaltyBps: import.meta.env.VITE_MAX_ROYALTY_BPS ?? process.env.VITE_MAX_ROYALTY_BPS,
-  taxAmount: import.meta.env.VITE_TAX_AMOUNT ?? process.env.VITE_TAX_AMOUNT
+  apiEndpoint: process.env.VITE_API_ENDPOINT,
+  network: process.env.VITE_BLOCK_NETWORK,
+  marketplaceAddress: process.env.VITE_MARKETPLACE_ADDRESS,
+  wZilAddress: process.env.VITE_WZIL_ADDDRESS,
+  cdnBaseUrl: process.env.VITE_CDN_BASE_URL,
+  maxRoyaltyBps: process.env.VITE_MAX_ROYALTY_BPS,
+  taxAmount: process.env.VITE_TAX_AMOUNT
 }
